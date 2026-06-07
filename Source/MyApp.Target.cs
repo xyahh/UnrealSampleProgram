9b70789cc71ef2a2c2f54394c123bae2c56003a0
Juan Marcelo Portillo <juan@xyah.net>
2026-06-08 04:41:03 +0900

    Removed forcing non-unity, etc.

--- Source/MyApp.Target.cs
+++ Source/MyApp.Target.cs
@@ -7,12 +7,6 @@ public class MyAppTarget : TargetRules
 {
 	public MyAppTarget(TargetInfo Target) : base(Target)
 	{
-		// Equivalent of RunUAT.bat BuildPlugin -StrictIncludes
-		// https://github.com/EpicGames/UnrealEngine/blob/5.5.0-release/Engine/Source/Programs/AutomationTool/Scripts/BuildPluginCommand.Automation.cs#L122-L126
-		bUseUnityBuild = false;
-		bUsePCHFiles = false;
-		bUseSharedPCHs = false;
-
 		Type = TargetType.Program;
 		LinkType = TargetLinkType.Monolithic;
 		DefaultBuildSettings = BuildSettingsVersion.Latest;
@@ -24,17 +18,9 @@ public class MyAppTarget : TargetRules
 			ExtraModuleNames.Add("EditorStyle");
 		}
 
-		// MyApp doesn't ever compile with the engine linked in
 		bCompileAgainstEngine = false;
-
-		// We need CoreUObject compiled in as the source code access module requires it
 		bCompileAgainstCoreUObject = true;
-
-		// MyApp.exe has no exports, so no need to verify that a .lib and .exp file was emitted by
-		// the linker.
 		bHasExports = false;
-
-		// Make sure to get all code in SlateEditorStyle compiled in
 		bBuildDeveloperTools = true;
 	}
 }
